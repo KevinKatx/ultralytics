5cf756f79167c57baf8c40a86df81428fef11d4a
KevinKatx <kenjibossing@gmail.com>
2026-02-09 19:16:36 +0800

    Different YOLOv8 CBAM Placements
    Pushing files for different placements of CBAM in YOLOv8

--- tests/Test.py
+++ tests/Test.py
@@ -1,3 +1,9 @@
 from ultralytics import YOLO
 model = YOLO(r'C:\Clone Repos\Modified-Ultralytics-for-CBAM-Implementation-of-YOLOv8\ultralytics\cfg\models\v8\yolov8-CBAM.yaml')  # build a new model from YAML
-results = model.train(data='coco128.yaml', epochs=3, imgsz=320)  # train the model
+results = model.train(data='Underwater.yaml', epochs=3, imgsz=320)  # train the model
+
+
+
+
+
+###Terminal Command to run the test file
